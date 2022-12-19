9fb284bb506cf51f49b6ae089296820466444f71
Muhammad Athallah <mhd.athallah@gmail.com>
2022-12-19 23:45:57 +0700

    feat: progress
    i'm tired

--- assets/codes/simpson/simpson_main.m
+++ assets/codes/simpson/simpson_main.m
@@ -4,7 +4,7 @@ format long
 minInf = -1e3;
 low = -1;
 high = 3;
-tol = 1e-7;
+tol = 1e-12;
 
 printf("\nKalkulasi z-Score Integration dengan Komposit Simpson")
 printf("\nSpesifikasi:")
@@ -28,8 +28,20 @@ diff = res - ansMinInf
 printf("\nHasil Integrasi dengan quadgk\n\n")
 [qAns, err] = quadgk(@z_norm_dist, low, high)
 
+printf("\nError dengan Hasil Integrasi melalui quadgk\n")
+err1 = abs(qAns - res);
+err2 = abs(qAns - ansMinInf);
+printf("\nP(a <= Z <= b): %d", err1)
+printf("\nP(Z <= b) - P(Z <= a): %d", err2)
+
 printf("\nHasil Integrasi dengan normcdf\n\n")
 cdf = normcdf([low high]);
 cdfAns = cdf(2) - cdf(1)
+
+printf("\nError dengan Hasil Integrasi melalui normcdf\n")
+err1 = abs(cdfAns - res);
+err2 = abs(cdfAns - ansMinInf);
+printf("\nP(a <= Z <= b): %d", err1)
+printf("\nP(Z <= b) - P(Z <= a): %d", err2)
 
 printf("\n")
