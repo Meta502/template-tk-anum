bc8c91c19d9d9d236b26e21e9a71d5542bc2a4c9
Muhammad Athallah <mhd.athallah@gmail.com>
2022-12-19 21:42:30 +0700

    update: simpson's code

--- assets/codes/simpson/simpson_main.m
+++ assets/codes/simpson/simpson_main.m
@@ -1,22 +1,34 @@
 pkg load statistics
+format long
 
-minInf = -1e5;
+minInf = -1e3;
 low = -1;
 high = 3;
-tol = 1e-4;
+tol = 1e-12;
 
-printf("\nCalculating z-Score Integration with Composite Simpson")
-printf("\nSpecifications:")
+printf("\nKalkulasi z-Score Integration dengan Komposit Simpson")
+printf("\nSpesifikasi:")
 printf("\nLower Limit: %d", low)
 printf("\nUpper Limit: %d", high)
-printf("\nTolerance Limit: %d\n\n", tol)
+printf("\nTolerance Limit: %d\n", tol)
 
+printf("\nHasil Integrasi dengan P(a <= Z <= b)\n")
 [res, n] = composite_simpson(low,high,tol,@z_norm_dist)
 
-printf("\nActual Result using quadgk\n\n")
+printf("\nHasil Integrasi dengan P(Z <= b) - P(Z <= a)\n")
+[resA, nA] = composite_simpson(minInf,low,tol,@z_norm_dist)
+[resB, nB] = composite_simpson(minInf,high,tol,@z_norm_dist)
+
+printf("\n")
+ansMinInf = abs(resB-resA)
+
+printf("\nPerbedaan antara P(a <= Z <= b) dan P(Z <= b) - P(Z <= a)\n")
+diff = res - ansMinInf
+
+printf("\nHasil Integrasi dengan quadgk\n\n")
 [qAns, err] = quadgk(@z_norm_dist, low, high)
 
-printf("\nActual Result using normcdf\n\n")
+printf("\nHasil Integrasi dengan normcdf\n\n")
 cdf = normcdf([low high]);
 cdfAns = cdf(2) - cdf(1)
 
